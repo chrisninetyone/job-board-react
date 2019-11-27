5831e9ce9e99115740241b4341aa4a33b5697ff5
Chris Foley <chris.foley@rfsmart.com>
2019-11-27 15:31:26 -0500

    basic component setup

---
+++ src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Job({job}) {
+    return (
+        <div className={'job'}>
+            <p>{job.title}</p>
+            <p>{job.company}</p>
+        </div>
+    )
+}
